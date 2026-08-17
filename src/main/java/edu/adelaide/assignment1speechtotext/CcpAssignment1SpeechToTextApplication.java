acec84321e7500c6dbe15dbf938466a33bb0522a
fikryjawhari <fikryaljawhari@gmail.com>
2026-08-17 10:46:23 +0930

    chore: initial project setup
    Spring Boot 4.1.0 project scaffold with Web, Actuator, Validation,
    and Reactive Web dependencies. Java 25.

---
+++ src/main/java/edu/adelaide/assignment1speechtotext/CcpAssignment1SpeechToTextApplication.java
@@ -0,0 +1,13 @@
+package edu.adelaide.assignment1speechtotext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcpAssignment1SpeechToTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcpAssignment1SpeechToTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/adelaide/assignment1speechtotext/CcpAssignment1SpeechToTextApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.adelaide.assignment1speechtotext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcpAssignment1SpeechToTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
